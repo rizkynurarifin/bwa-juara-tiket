96d11e115b40e0cca2be1eb1bcbfa8b244101520
Rizky <arifinnurrizky@gmail.com>
2025-06-23 22:18:50 +0700

    Initial Laravel project

---
+++ app/Models/BookingTransaction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BookingTransaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'booking_trx_id',
+        'phone_number',
+        'email',
+        'proof',
+        'total_amount',
+        'total_participant',
+        'is_paid',
+        'started_at',
+        'ticket_id'
+    ];
+
+    public function ticket(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class, 'ticket_id');
+    }
+}
